36d5d70c516fe4fd550347fbc5a48491e42f1234
DKajuju <dianakajuju2@gmail.com>
2024-02-17 13:22:00 +0300

    add concationation logic

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ let num1= 3;
 let num2= 2;
 let shopping = ['bread', 'sugar', 'milk']
 let solution = num1 * num2
-let name = "kib";
+let name = "Diana";
 
-let newMessage = message+`${message} + my name is ${name}.`;
+let newMessage = `${message} my name is ${name}.`;
 let sum = num1 / num2;
 
 console.log(solution);
