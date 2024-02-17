8f63669e02d1c76eaeb54b9fc471b1215b7b0d8c
DKajuju <dianakajuju2@gmail.com>
2024-02-17 13:17:14 +0300

    add concatination logic

--- script.js
+++ script.js
@@ -2,9 +2,14 @@ let message = "Hello Zindua!";
 let num1= 3;
 let num2= 2;
 let shopping = ['bread', 'sugar', 'milk']
+let solution = num1 * num2
+let name = "kib";
 
+let newMessage = message+`${message} + my name is ${name}.`;
 let sum = num1 / num2;
 
 console.log(solution);
 console.log(shopping);
+console.log(newMessage);
+
 
